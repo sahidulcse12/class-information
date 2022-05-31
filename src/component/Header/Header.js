2d83979bc371edc2815c9cbfcc45a7ba67d30744
sahidulcse12 <m.sahidulislam.14@gmail.com>
2022-05-31 14:56:27 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">ClassFication</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <NavDropdown title="" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Registration</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
